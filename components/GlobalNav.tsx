3601947d857df8b8c980546ebd492cd66c48f0ef
shovin45 <shovin36@gmail.com>
2021-09-25 11:09:46 +0900

    add global nav animation

--- components/GlobalNav.tsx
+++ components/GlobalNav.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+
+import style from '../styles/GlobalNav.module.scss'
 
 type Props = {
   className?: string
@@ -25,18 +28,24 @@ const nav = [
 ]
 
 const GlobalNav: React.FC<Props> = ({ className }) => {
+  const { pathname } = useRouter()
   return (
     <nav className='relative z-50'>
       <ul
-        className={`${className} absolute top-0 right-0 flex justify-end pt-2 sm:pt-4 px-5 sm:px-24 w-full `}
+        className={`${className} absolute top-0 right-0 flex justify-end pt-6 sm:pt-9 px-5 sm:px-24 w-full `}
       >
         {nav.map(({ name, href }) => {
           return (
             <li
-              className='p-3 sm:p-5 last:pr-0 text-xs sm:text-base font-bold'
+              className={`mx-1 px-2 sm:px-4 last:-mr-2 sm:last:-mr-4 text-xs sm:text-base font-bold ${
+                style.button
+              } ${pathname == href ? style.active : ''}`}
               key={name}
+              tabIndex={0}
             >
-              <Link href={href}>{name}</Link>
+              <Link href={href}>
+                <a tabIndex={-1}>{name}</a>
+              </Link>
             </li>
           )
         })}
