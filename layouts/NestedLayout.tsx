bdfaa140e2775ea268c2b6f1461dfa07fde6b430
shovin45 <shovin36@gmail.com>
2021-09-25 10:14:36 +0900

    logo add root link in nestedLayout

--- layouts/NestedLayout.tsx
+++ layouts/NestedLayout.tsx
@@ -1,16 +1,21 @@
 import GlobalNav from '../components/GlobalNav'
 import Footer from '../components/Footer'
 import Image from 'next/image'
+import Link from 'next/link'
 import type { NextPage } from 'next'
 
 const Layout: NextPage = ({ children }) => {
   return (
     <>
       <GlobalNav />
-      <div className='absolute z-10 p-2 -m-10'>
-        {/* <img src='/home-img.png' alt='' width={121} height={97} /> */}
-        <Image src='/home-img.png' alt='' width={121} height={97} />
+      <div className='absolute z-50 p-2 -m-10'>
+        <Link href='/'>
+          <a className='block'>
+            <Image src='/home-img.png' alt='' width={121} height={97} />
+          </a>
+        </Link>
       </div>
+
       <main>{children}</main>
       <Footer />
     </>
